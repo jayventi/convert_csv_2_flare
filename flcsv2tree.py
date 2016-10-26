133b4d0f91a3d89794d5c0d45a5725454319dfa7
Jay Venti <jayventi@gmail.com>
2016-10-26 11:01:26 -0700

    added main orchestration function convert_csv_2_flare

--- flcsv2tree.py
+++ flcsv2tree.py
@@ -1,6 +1,16 @@
-#flcsv2tree.py
+"""
+Convert Storagetats csv output to D3 Tree Map graphics flare.json format
+StorageStats: Convert FileSystem Storage Statistics csv output to
+a hierarchically embedded treee format used by mbostocks D3 treemap
+referred to as flare.json see:
+http://mbostock.github.io/d3/talk/20111018/treemap.html
 
-#import csv
+python
+Created on Oct 26, 2016
+@author: jayventi
+flcsv2tree.py convert_csv_2_flare ???
+"""
+
 from csv import DictReader
 import os.path
 from json import dump
@@ -21,6 +31,11 @@ def RepresentsInt(s):
 
 
 def csv_2_tree_path_dicts(file):
+    """
+    opens storgstats csv file and setsup in path_dicts format
+    opens csv output file from storgstats.py converge to a dictionary
+    with the key is row['path'] and the value is row
+    """
     path_ditc = {}
     with open(file) as csvfile:
         reader = DictReader(csvfile)
@@ -33,10 +48,13 @@ def csv_2_tree_path_dicts(file):
 
 def write_2_jsonfile(pydict, pathfile_name):
     with open(pathfile_name, 'w') as outfile:
-        dump(dir_tree, outfile)
+        dump(pydict, outfile)
 
 
 def root_str(candidate, test):
+    """
+    finds shortest common prefix string
+    """
     min_idx = min(len(candidate), len(test))
     for i in range(min_idx):
         if candidate[i] != test[i]:
@@ -45,6 +63,11 @@ def root_str(candidate, test):
 
 
 def find_root_dir(tree_ditc):
+    """
+    finds common base path to all listed full paths
+    finds shortest common prefix string which will
+    be equivalent to root / base path
+    """
     root_dir_candidate = tree_ditc.keys()[0]
     for test_path in tree_ditc.keys():
         root_dir_candidate = root_str(root_dir_candidate, test_path)
@@ -53,11 +76,18 @@ def find_root_dir(tree_ditc):
 
 
 def add_root(tree_ditc, root_path):
+    """
+    ads a empty root directory, totals added later
+    Storagetats does not output the root directory this routine remedies
+    """
     tree_ditc[root_path] = {}
     tree_ditc[root_path]['root'] = True
 
 
 def calc_children(tree_ditc):
+    """
+    calculates list of sub directories for each and addeds
+    """
     for each_dir_path, dir_info in tree_ditc.items():
         this_dir = os.path.split(each_dir_path)[-1]
         parent_dir = os.path.dirname(each_dir_path)
@@ -71,6 +101,11 @@ def calc_children(tree_ditc):
 
 
 def calc_root_totlas(tree_ditc, root_path):
+    """
+    calculates totals for root directory
+    only needs to sum up from immediate children since they
+    contain all of their subtotals down to the leafs
+    """
     # TODO ??
     root_dir = root_path
     for child in tree_ditc[root_dir]['children']:
@@ -88,6 +123,11 @@ def calc_root_totlas(tree_ditc, root_path):
 
 
 def working_node_total(working_node, fl_type):
+    """
+    calculates total directory file size or file count 
+    if '_' appears in fl_type total '_Cn' values
+    otherwise total file size types
+    """
     total = 0
     # setup typ_suf as fun(fl_type)
     typ_suf = ''
@@ -109,7 +149,19 @@ def working_node_total(working_node, fl_type):
 
 
 def build_dir_tree(tree_ditc, working_node, fl_type, level, max_level):
-    # TODO total, max_level
+    """
+    Main algorithm for converting data structure to flare.json format
+    recursively parses row-based dictionary tree_ditc producing embedded
+    hierarchically tree representation of data.
+    Takes row-based dir data from tree_ditc, works on the current
+    working_node converts it into a directory format
+    {'name':dir-path,"children":[list of children]} if no children replace
+    children with "size": integer value.
+    max_level: Describes the maximum depth returned
+    fl_type: is intended to be a str name of one of the values totaled in
+    Storagetats csv output. If 'total' in fl_type totals are calculated
+    see working_node_total.
+    """
     build = {}
     level += 1
     name = working_node['name']
@@ -119,7 +171,7 @@ def build_dir_tree(tree_ditc, working_node, fl_type, level, max_level):
         build["children"] = []
         for child in children:
             #print 'name', name, 'child', child, 'level', level
-            path_key = name + '/' + child 
+            path_key = name + '/' + child
             next_working_node = {'name': path_key, 'node': tree_ditc[path_key]}
             build["children"].append(
                 build_dir_tree(tree_ditc, next_working_node, fl_type, level, max_level))
@@ -132,37 +184,40 @@ def build_dir_tree(tree_ditc, working_node, fl_type, level, max_level):
     return build
 
 
-#######################
-print 'Start convert csv to flare json'
-# lode data from csv file
-tree_ditc = csv_2_tree_path_dicts('FSHistory_Dojo.csv')  # FSHistory_Dojo
-# calculate commoun base path
-root_path = find_root_dir(tree_ditc)
-# add a root node
-add_root(tree_ditc, root_path)
-# calculate list of children for each node and add to each
-calc_children(tree_ditc)
-# calculate values totals for root by summing up child values
-calc_root_totlas(tree_ditc, root_path)
+def convert_csv_2_flare(csv_in, json_out, root_path=None):
+    print 'Start convert csv to flare json'
+    # lode data from csv file
+    tree_ditc = csv_2_tree_path_dicts(csv_in)  # FSHistory_Dojo    
+    if root_path is None:
+        # calculate commoun base path
+        root_path = find_root_dir(tree_ditc)
+    # add a root node
+    add_root(tree_ditc, root_path)
+    # calculate list of children for each node and add to each
+    calc_children(tree_ditc)
+    # calculate values totals for root by summing up child values
+    calc_root_totlas(tree_ditc, root_path)
+    # build d3 treemap comparable data structured
+    # stare with root node
+    working_node = {'name': root_path, 'node': tree_ditc[root_path]}
+    level = 0
+    dir_tree = build_dir_tree(tree_ditc, working_node, 'total', level, 4)
+    # out put dir_tree as json file
+    write_2_jsonfile(dir_tree, json_out)
+    print 'Done convert csv to flare json'
 
-# build d3 treemap comparable data structured
-# stare with root node
-working_node = {'name': root_path, 'node': tree_ditc[root_path]}
-level = 0
-dir_tree = build_dir_tree(tree_ditc, working_node, 'total', level, 4)
-
-# out put dir_tree as json file
+    #######################
+    #'FSHistory_Dojo.csv'
 
 
-write_2_jsonfile(dir_tree, 'flare_data.json')
-print 'Done convert csv to flare json'
+convert_csv_2_flare('FSHistory_Dojo.csv', 'flare_data.json', "C:/work/Dojo")
 
 '''
 TODO:
-    git
+    xx git
     oo 1
     unittest 1
-    maim > convert_csv_2_flare(csv_in, json_out, root_path = null) 1.5
+    xx maim > convert_csv_2_flare(csv_in, json_out, root_path = null) 1.5
     readme.md 1.5
     github as website index.html... 2
     leve 3 shows ???
